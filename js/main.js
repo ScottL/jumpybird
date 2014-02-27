dc13ed0262ab767d1fcab3ef71753ec6182336ce
Scott Lin <11ScottL5@gmail.com>
2014-02-26 23:18:03 -0800

    Javascript skeleton code

--- js/main.js
+++ js/main.js
@@ -2,10 +2,35 @@
 
 */
 
-
 $(document).ready(function(){
 
+// setup and initilize
 
+// show ready / splash screem
+	startScreen();
 
 });
 
+function startScreen(){
+
+}
+
+function click(){
+	// mouse click makes the sprite jump
+	jump();
+	// or start the game, if game hasnt started
+	gameStart();
+}
+
+function gameStart(){
+
+}
+
+function jump(){
+	// action of sprite will be here
+}
+
+function gameThread(){
+	// main thread to loop the game
+
+}
