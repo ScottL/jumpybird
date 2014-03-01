0427154043ea6cb7319c3ed19bdf9f222ab7bde6
Scott Lin <11ScottL5@gmail.com>
2014-02-28 22:53:00 -0800

    mostly main.js. add more to skeleton

--- js/main.js
+++ js/main.js
@@ -1,37 +1,62 @@
 /* main.js
 
 */
+var startScreenState = 0;
+var gameRunningState = 1;
 
+var currentState;
+
+var ground = document.getElementById("ground");
+var ceiling = document.getElementById("ceiling");
+var sprite = document.getElementById("sprite");
+ 
+var position = 280;
+var speed = 0;
+var jump = -10;
+var gravity = 2;
+
+$(document).on("mousedown", leftClick);
 $(document).ready(function(){
 	console.log( "ready!" );
 	// setup and initilize
-
+	// show start / splash screen
 	
-	// show ready / splash screem
 	startScreen();
 });
 
+
 function startScreen(){
+	currentState = startScreenState;
 	//$('ground').addClass('notransition'); // to remove transition
 	//$('ceiling').addClass('notransition'); // to remove transition
-	//var elem = document.getElementById("ground");
-	//elem.style.webkitAnimationPlayState = 'paused';
+	
+	ground.style.webkitAnimationPlayState = 'running';
+	ceiling.style.webkitAnimationPlayState = 'running';
+	sprite.style.webkitAnimationPlayState = 'paused';
 
 }
 
-function click(){
-	// mouse click makes the sprite jump
-	jump();
-	// or start the game, if game hasnt started
-	gameStart();
+function leftClick(){
+	console.log( "screen click!" );
+	
+	// start game if still on start / splash screen
+	// perform jump action if game is running
+	if (currentState == startScreenState){
+		gameStart();
+	} else if (currentState == gameRunningState){
+		jumpUp();
+	}
 }
 
 function gameStart(){
-
+	currentState = gameRunningState;
 }
 
-function jump(){
+function jumpUp(){
 	// action of sprite will be here
+	
+	
+	sprite.style.top = position;
 }
 
 function gameThread(){
